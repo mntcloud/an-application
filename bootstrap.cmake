8b0f5dae567e86332d4351a01782ee364a21b1b9
mntcloud <40216914+mntcloud@users.noreply.github.com>
2023-05-04 18:22:24 +0300

    Add useful messages and add command to remove .git folder in the end

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -23,6 +23,8 @@ configure_file(main/src/main.cc.in main/src/main.cc)
 
 message(STATUS "cleaning everything up...")
 
+message(STATUS "removing template files...")
+
 file(REMOVE CMakeLists.txt.in)
 file(REMOVE src/version.cc.in)
 
@@ -31,5 +33,10 @@ file(REMOVE tests/CMakeLists.txt.in)
 file(REMOVE main/CMakeLists.txt.in)
 file(REMOVE main/src/main.cc.in)
 
+message(STATUS "removing .git folder...")
+
+file(REMOVE_RECURSE .git/)
+
 message(STATUS "DONE!")
 message(STATUS "To build your project run: cmake -S main -B build/main")
+message(STATUS "Feel free to delete me!")
